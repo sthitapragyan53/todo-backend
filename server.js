ea12fc287b67f1f42aa913d025e0e80d0c5a854c
sthita <sthitapragyan8353@gmail.com>
2025-11-13 00:11:49 +0530

    Fix final Render CORS config

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",               // Local frontend
-    "https://todo-frontend.onrender.com"   
+    "https://todo-frontend.onrender.com"  
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
